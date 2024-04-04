ffb7911f58c161fc5a6f5950ecc389a692e4c063
Joseph Adoga <adoga.joseph0@gmail.com>
2024-04-04 14:13:51 -0500

    Added Functions for get System Information

--- assets/script/app.js
+++ assets/script/app.js
@@ -14,6 +14,8 @@ const blurCover = select('.cover');
 const acceptButton = select('.accept');
 const settingsButton = select('.settings');
 
+const { log } = console;
+
 function showCookieDialog() {
     blurCover.classList.add('isvisible');
     cookieDialog.classList.add('isvisible');
@@ -27,6 +29,43 @@ function hideCookieDialog() {
 function showSettingsDialog() {
     blurCover.classList.add('isvisible');
     settingsDialog.classList.add('isvisible');
+}
+
+function getBrowser() {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    
+    const browserMap = {
+        "edg": "Microsoft Edge",
+        "opr": "Opera",
+        "chrome": "Chrome",
+        "firefox": "Firefox",
+        "safari": "Safari",
+        "msie": "Internet Explorer",
+        "trident": "Internet Explorer"
+    };
+
+    const browser = Object.keys(browserMap).find(key => userAgent.includes(key));
+    return browser ? browserMap[browser] : "Null";
+}
+
+function getOS() {
+    if (window.navigator.userAgent.indexOf("Windows") != -1) {
+        return "Windows";
+    } else if (window.navigator.userAgent.indexOf("Mac OS") != -1) {
+        return "Mac OS";
+    } else if (window.navigator.userAgent.indexOf("Linux") != -1) {
+        return "Linux";
+    } else {
+        return "Null";
+    }
+}
+
+function getScreenWidth() {
+    return window.screen.width;
+}
+
+function getScreenHeight() {
+    return window.screen.height;
 }
 
 listen('load', window, function() {
